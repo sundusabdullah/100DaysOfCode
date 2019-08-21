14b25f891b505332e0f165adc5aa1a3a614eb01b
sundus <s.alghernas1414@hotmail.com>
2019-08-21 05:40:01 +0300

    last update

--- 100DaysOfCode.py
+++ 100DaysOfCode.py
@@ -2,7 +2,12 @@
 
 # First Day...
 
+#the print method uses to print anything on the screen
 print("Hello World")
 
+"""
+can use Relational
+Operators in python
+"""
 if 5 > 2:
     print("Five is greater than two!")
