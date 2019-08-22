8e1e5a2dbcddc33544fa21d85469511c46681996
sundus <s.alghernas1414@hotmail.com>
2019-08-22 04:48:39 +0300

    last update

--- 100DaysOfCode.py
+++ 100DaysOfCode.py
@@ -1,6 +1,6 @@
 
 
-# First Day...
+# First/ Second Day...
 
 #the print method uses to print anything on the screen
 print("Hello World")
@@ -11,3 +11,32 @@ Operators in python
 """
 if 5 > 2:
     print("Five is greater than two!")
+
+
+#Third day
+
+#Python has no command for declaring a variable
+
+x = 5 # x is integer
+y = "Sundu" # y is string
+print(x)
+print(y)
+
+#In Python single or double quotes is same
+
+y = 'Sundus'
+z = "Sundus"
+
+#Assign Value to Multiple Variables
+x,y,z = "sundus", "Abdullah", "IT"
+print(x,y,z)
+
+#Output Variables
+x = "awesome"
+print("python is " + x)
+
+#or, To use combine + must variables have the same type
+
+x = "awesome"
+y = "python is "
+print(y + x)
