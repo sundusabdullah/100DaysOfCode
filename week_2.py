52ae7e8b1ad2b6fba9005036060f6ba30d14564a
sundus <s.alghernas1414@hotmail.com>
2019-08-25 23:25:15 +0300

    last update

---
+++ week_2.py
@@ -0,0 +1,29 @@
+#Sixth Day
+
+# All outputs become int type
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+
+print(x,"\n", y,"\n",z)
+
+
+
+# All outputs become float type
+
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+
+print(x,"\n", y,"\n",z,"\n",w)
+
+
+# All outputs become string type
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+
+print(x,"\n", y,"\n",z)
