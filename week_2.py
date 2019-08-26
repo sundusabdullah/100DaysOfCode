b1d9225638e6c65499f927f39c17560e70d1e3cd
sundus <s.alghernas1414@hotmail.com>
2019-08-26 04:53:53 +0300

    last update

--- week_2.py
+++ week_2.py
@@ -1,13 +1,11 @@
-#Sixth Day
+# Sixth Day
 
 # All outputs become int type
 
 x = int(1)
 y = int(2.8)
 z = int("3")
-
 print(x,"\n", y,"\n",z)
-
 
 
 # All outputs become float type
@@ -16,7 +14,6 @@ x = float(1)
 y = float(2.8)
 z = float("3")
 w = float("4.2")
-
 print(x,"\n", y,"\n",z,"\n",w)
 
 
@@ -25,5 +22,37 @@ print(x,"\n", y,"\n",z,"\n",w)
 x = str("s1")
 y = str(2)
 z = str(3.0)
-
 print(x,"\n", y,"\n",z)
+
+
+
+
+
+# Seventh Day
+
+# To display a string use print() function
+# will print same result
+print('Hello')
+print("Hello")
+
+# Assign String to a Variable
+
+a = "Hello"
+print(a) # output = Hello
+
+#  Multiline Strings
+
+a = '''
+Python is an interpreted, object-oriented, high-level programming
+ language with dynamic semantics. Its high-level built 
+ in data structures
+'''
+
+print(a)
+
+
+a = "Hello, World!"
+
+print(a[1]) # output = e
+print(a[2:5]) # output = llo
+
