2c9237e767ee30e1b3511b93d2167660a8ff2e17
sundus <s.alghernas1414@hotmail.com>
2019-09-02 01:09:38 +0300

    last update

---
+++ week_3.py
@@ -0,0 +1,36 @@
+# Day 13
+
+s = []
+print(s) # empty List
+
+numbers = [1,2,3,4,5]
+print(numbers) #list contains numbers
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist) #list contains string
+
+list = ["apple", "banana", "cherry",1,2,3] #list contains string and number
+print(list)
+
+num_list = [1.5, 2.5, 3.5]
+print(num_list)
+print(num_list[1]) #to access items
+
+
+thislist = ["apple", "banana", "cherry"]
+#use for loop to print all item
+for i in thislist:
+    print(i)
+
+nums_lise = [1.5, 2.5, 3.5, 1,2,3]
+
+for n in nums_lise:
+    print(n)
+
+# Change item in list
+thislist[1] = "blackcurrant"
+print(thislist)
+
+# delete item in list
+del thislist[1]
+print(thislist)
