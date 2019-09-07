a5cb09f4cdf6798a8a15081d3540ed8948b9a0f8
sundus <s.alghernas1414@hotmail.com>
2019-09-08 00:36:16 +0300

    last update

--- week_4.py
+++ week_4.py
@@ -26,3 +26,24 @@ print(thisset_2)
 # To add more than one item use update()
 thisset_2.update(["orange", "mango", "grapes"])
 print(thisset_2)
+
+
+
+
+# Day_21
+
+# Length of a Set
+print(len(thisset_2))
+
+# Remove Item use discard() or remove(), pop()
+thisset_2.remove("banana")
+print(thisset_2)
+
+# clear() method empties the set
+thisset_2.clear()
+print(thisset_2)
+
+# set() Constructor
+
+thisset_3 = set(("apple", "banana", "cherry"))
+print(thisset_3)
