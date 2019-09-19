37761a8e582f7056c32781217dc5e271b0dfc5b2
sundus <s.alghernas1414@hotmail.com>
2019-09-19 04:25:14 +0300

    last update

--- month_2/week_1.py
+++ month_2/week_1.py
@@ -185,3 +185,18 @@ def my_function(country = "Norway"):
 
 my_function("Saudi Arabia ")
 my_function()
+
+# Day_32_33
+
+A = [3, 5, 7, 9, 11, 13, 15, 17]
+B = [2, 4, 6, 8, 10, 12, 14, 16]
+
+for x in A:
+    for y in B:
+        print(x, y)
+
+# Another way
+
+for x in range(3, 18, 2):
+    for y in range(2, 17, 2):
+        print(x, y)
