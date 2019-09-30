ff4110c4d299cd329a13548b599e8f034ac0fa82
sundus <s.alghernas1414@hotmail.com>
2019-10-01 00:53:09 +0300

    last update

--- month_2/week_3.py
+++ month_2/week_3.py
@@ -130,3 +130,73 @@ x = Student("sundus", "Abdullah")
 x.printname()
 
 # Day_44
+
+# super() Function
+
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+    def printname(self):
+       print(self.firstname, self.lastname)
+
+class Student(Person):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname)
+
+x = Student("sundus", "Abdullah")
+x.printname()
+
+# Add Properties
+
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+    def printname(self):
+       print(self.firstname, self.lastname)
+
+class Student(Person):
+    def __init__(self, fname, lname):
+        super().__init__(fname, lname)
+        self.graduationyear = 2018
+
+x = Student("sundus", "Abdullah")
+print(x.graduationyear)
+
+# Add parameter
+
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+    def printname(self):
+       print(self.firstname, self.lastname)
+
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+
+x = Student("sundus", "Abdullah", 2018)
+print(x.graduationyear)
+
+# Add Methods
+
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+    def printname(self):
+       print(self.firstname, self.lastname)
+
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+
+    def welcome(self):
+        print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+x = Student("sundus", "Abdullah", 2018)
+x.welcome()
