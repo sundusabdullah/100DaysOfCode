7cb08ee0013aee57322e1218c050eba2c449c274
sundus <s.alghernas1414@hotmail.com>
2019-10-06 02:39:14 +0300

    last update

--- month_2/week_4.py
+++ month_2/week_4.py
@@ -23,3 +23,34 @@ def myFunc():
     print(x)
 myFunc()
 print(x)
+
+# Day_49
+
+# Naming Variables
+
+x = 300 # Global
+
+def myFunc():
+    x = 200 #local
+    print(x)
+myFunc()
+print(x)
+
+# Global Keyword
+
+def myFunc():
+    global x
+    x = 300
+myFunc()
+print(x)
+
+# change to a global variable
+
+x = 300
+
+def myfunc():
+    global x
+    x = 200
+
+myfunc()
+print(x)
