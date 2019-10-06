93d1f415e859168fb6106b0e4f3f72ee1d317ba5
sundus <s.alghernas1414@hotmail.com>
2019-10-07 00:13:50 +0300

    last update

---
+++ month_2/mymodule.py
@@ -0,0 +1,9 @@
+
+def greeting(name):
+    print("Hello"+ name)
+
+person1 = {
+    "name": "Sundus",
+    "age":  26,
+    "country": "Saudi Arabia"
+}
--- month_2/week_4.py
+++ month_2/week_4.py
@@ -1,56 +1,73 @@
-# Day_48
+# # Day_48
+#
+# #  Local Scope
+#
+# def myFunc():
+#     x = 300
+#     print(x)
+# myFunc()
+#
+# # Function Inside Function
+# def myFunc():
+#     x = 300
+#     def myinnerfunc():
+#         print(x)
+#
+#     myinnerfunc()
+# myFunc()
+#
+# # Global Scope
+#
+# x = 300
+# def myFunc():
+#     print(x)
+# myFunc()
+# print(x)
+#
+# # Day_49
+#
+# # Naming Variables
+#
+# x = 300 # Global
+#
+# def myFunc():
+#     x = 200 #local
+#     print(x)
+# myFunc()
+# print(x)
+#
+# # Global Keyword
+#
+# def myFunc():
+#     global x
+#     x = 300
+# myFunc()
+# print(x)
+#
+# # change to a global variable
+#
+# x = 300
+#
+# def myfunc():
+#     global x
+#     x = 200
+#
+# myfunc()
+# print(x)
 
-#  Local Scope
+# Day_50
 
-def myFunc():
-    x = 300
-    print(x)
-myFunc()
 
-# Function Inside Function
-def myFunc():
-    x = 300
-    def myinnerfunc():
-        print(x)
+# Create a Module: Save code in file .py
+# Use a Module: using by import statement after to save file of code
+import mymodule
+mymodule.greeting(" Sundus")
 
-    myinnerfunc()
-myFunc()
+# Variables in Module
 
-# Global Scope
+import mymodule
 
-x = 300
-def myFunc():
-    print(x)
-myFunc()
-print(x)
+a = mymodule.person1["age"]
+print(a)
 
-# Day_49
 
-# Naming Variables
-
-x = 300 # Global
-
-def myFunc():
-    x = 200 #local
-    print(x)
-myFunc()
-print(x)
-
-# Global Keyword
-
-def myFunc():
-    global x
-    x = 300
-myFunc()
-print(x)
-
-# change to a global variable
-
-x = 300
-
-def myfunc():
-    global x
-    x = 200
-
-myfunc()
-print(x)
