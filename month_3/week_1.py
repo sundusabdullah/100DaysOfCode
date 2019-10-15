298c6ce86683b8e6a76cfa4b2d56d8ed321084ab
sundus <s.alghernas1414@hotmail.com>
2019-10-15 06:10:38 +0300

    last update

--- month_3/week_1.py
+++ month_3/week_1.py
@@ -60,3 +60,50 @@ print(json.dumps(x, separators=(". ", " = ")))
 
 # Use the sort_keys
 print(json.dumps(x, sort_keys=True))
+
+
+# Day_57
+
+# RegEx Module
+
+import re
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+
+if(x):
+    print("Yes, We have a match!")
+else:
+    print("No match")
+
+
+# Metacharacters
+y = re.findall("[a-f]", txt)
+print(y)
+
+z = re.findall("\S", txt)
+print(z)
+
+S = re.findall("\W", txt)
+print(S)
+
+# Day_58
+
+# findall() Function
+import re
+txt = "The rain in Spain"
+x = re.findall("ai", txt)
+print(x)
+
+if(x):
+    print("Yes, there is at least one match")
+else:
+    print("No match")
+
+# search() Function
+
+y = re.search("\s", txt)
+print("The first white-space character is located in position:", y.start())
+
+z = re.split("\s", txt)
+print(z)
