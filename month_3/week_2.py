08cacb2335fac6d14eb22797105916d9834c4db7
sundus <s.alghernas1414@hotmail.com>
2019-10-20 23:50:29 +0300

    last update

---
+++ month_3/week_2.py
@@ -0,0 +1,28 @@
+
+# Day_62
+
+
+# PIP is Installed
+#  If you have Python version 3.4 or later, PIP is included by default.
+
+# Using a Package
+import camelcase
+c = camelcase.CamelCase()
+txt = "lorem ipsum dolor sit amet"
+print(c.hump(txt))
+
+# Remove a Package
+# Use uninstall to remove a package
+
+#  List Packages
+# Use Pip list
+
+
+
+# Day_63
+
+# Command Line Input
+
+print("Enter your name: ")
+x = input()
+print("Hello", x)
