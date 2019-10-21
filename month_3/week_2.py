992ba32f38ef8377cbc74d391dc61182483a0eaf
sundus <s.alghernas1414@hotmail.com>
2019-10-21 23:55:39 +0300

    last update

--- month_3/week_2.py
+++ month_3/week_2.py
@@ -26,3 +26,42 @@ print(c.hump(txt))
 print("Enter your name: ")
 x = input()
 print("Hello", x)
+
+# Day_64
+
+# Exception Handling
+try:
+    print(x)
+except:
+    print("An exception occurred")
+
+# This statement will raise an error, because x is not defined
+print(x)
+
+# Many Exceptions
+try:
+    print(x)
+except NameError:
+    print("Variable x is not defined")
+except:
+    print("Something else went wrong")
+
+# Else
+
+try:
+    print("Hello")
+except:
+    print("Something else went wrong")
+else:
+    print("Nothing went wrong")
+
+
+# Finally
+
+try:
+    print(x)
+except:
+    print("Something  went wrong")
+finally:
+    print("The 'try except' is finished")
+
