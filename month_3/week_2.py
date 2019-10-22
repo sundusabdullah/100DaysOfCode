38cc8d1b391da1d99e77efee102abfc2970baf1a
sundus <s.alghernas1414@hotmail.com>
2019-10-22 18:56:39 +0300

    last update

--- month_3/week_2.py
+++ month_3/week_2.py
@@ -1,7 +1,6 @@
 
 # Day_62
 
-
 # PIP is Installed
 #  If you have Python version 3.4 or later, PIP is included by default.
 
@@ -85,4 +84,20 @@ quantity = 3
 itemno = 567
 price = 49
 myorder = "I want {} pieces of item number {} for {:.2f} dollars."
-print(myorder.format(quantity, itemno, prince))
+print(myorder.format(quantity, itemno, price))
+
+
+# Day_66
+
+# Index numbers
+
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+# Named Indexes
+
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname="Ford", model="Mustang"))
