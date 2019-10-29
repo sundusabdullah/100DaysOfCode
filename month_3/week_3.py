73eb07923c810150209943de7c51cec52ac46fe2
sundus <s.alghernas1414@hotmail.com>
2019-10-30 01:01:50 +0300

    last update

--- month_3/week_3.py
+++ month_3/week_3.py
@@ -1,57 +1,57 @@
-
-# day_69
-
-#  open() function
-f = open("hello.txt", "rt")
-
-# Open a File on the Server
-
-f = open("hello.txt", "r")
-print(f.read())
-
-# Read Only Parts of the File
-
-print(f.read(5))
-
-# Read Lines
-print(f.readline())
-
-# Close Files
-f.close()
-
-# Write to an Existing File
-
-f = open("hello.txt", "a")
-f.write("Now this file has more content!")
-f.close()
-
-f = open("hello.txt", "r")
-print(f.read())
-
-# Create a New File
-f = open("myfile.txt", "x")
-
-# Delete a File
-import os
-os.remove("myfile.txt")
-
-# Check if File exist
-
-if os.path.exists("myfile.txt"):
-    os.remove("myfile.txt")
-else:
-    print("The file does not exite")
-
-# Delete Folder
-
-os.rmdir("myfolder")
-
-
-# day_70
-
+#
+# # day_69
+#
+# #  open() function
+# f = open("hello.txt", "rt")
+#
+# # Open a File on the Server
+#
+# f = open("hello.txt", "r")
+# print(f.read())
+#
+# # Read Only Parts of the File
+#
+# print(f.read(5))
+#
+# # Read Lines
+# print(f.readline())
+#
+# # Close Files
+# f.close()
+#
+# # Write to an Existing File
+#
+# f = open("hello.txt", "a")
+# f.write("Now this file has more content!")
+# f.close()
+#
+# f = open("hello.txt", "r")
+# print(f.read())
+#
+# # Create a New File
+# f = open("myfile.txt", "x")
+#
+# # Delete a File
+# import os
+# os.remove("myfile.txt")
+#
+# # Check if File exist
+#
+# if os.path.exists("myfile.txt"):
+#     os.remove("myfile.txt")
+# else:
+#     print("The file does not exite")
+#
+# # Delete Folder
+#
+# os.rmdir("myfolder")
+#
+#
+# # day_70
+#
 import mysql.connector
-
-# creating a connection
+#
+# # creating a connection
 
 mydb = mysql.connector.connect(
 
@@ -62,94 +62,121 @@ mydb = mysql.connector.connect(
 )
 print(mydb)
 
-# Creating a Database
+# # Creating a Database
 mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE mydatabase")
+# mycursor.execute("CREATE DATABASE mydatabase")
+#
+# # Check if Database Exists
+# mycursor.execute("SHOW DATABASES")
+# for x in mycursor:
+#     print (x)
+#
+# # Creating a Table
+# mycursor = mydb.cursor()
+# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+#
+# # Check if Table Exists
+# mycursor.execute("SHOW TABLES")
+# for x in mycursor:
+#     print (x)
+#
+# # Primary Key
+# mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+#
+#
+# # Day_71
+#
+# # Insert Into Table
+#
+# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+# val = ("John", "Highway 21")
+# mycursor.execute(sql, val)
+# mydb.commit()
+# print(mycursor.rowcount, "record inserted.")
+#
+# # Insert Multiple Rows
+# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+# val = [
+#   ('Peter', 'Lowstreet 4'),
+#   ('Amy', 'Apple st 652'),
+#   ('Hannah', 'Mountain 21'),
+#   ('Michael', 'Valley 345'),
+#   ('Sandy', 'Ocean blvd 2'),
+#   ('Betty', 'Green Grass 1'),
+#   ('Richard', 'Sky st 331'),
+#   ('Susan', 'One way 98'),
+#   ('Vicky', 'Yellow Garden 2'),
+#   ('Ben', 'Park Lane 38'),
+#   ('William', 'Central st 954'),
+#   ('Chuck', 'Main Road 989'),
+#   ('Viola', 'Sideway 1633')
+# ]
+#
+# mycursor.executemany(sql, val)
+# mydb.commit()
+# print(mycursor.rowcount, "was inserted.")
+#
+# # Get Inserted ID
+# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+# val = ("Sundus", "Highway 21")
+# mycursor.execute(sql, val)
+# mydb.commit()
+# print("1 record inserted, ID:", mycursor.lastrowid)
+#
+# # Select From
+# mycursor.execute("SELECT * FROM customers")
+# myresult = mycursor.fetchall()
+# for x in myresult:
+#     print(x)
+#
+# # Selecting Columns
+# mycursor.execute("SELECT name, address FROM customers")
+# myresult = mycursor.fetchall()
+# for x in myresult:
+#     print(x)
+#
+# # fetchone() Method
+# mycursor.execute("SELECT * FROM customers")
+# myresult = mycursor.fetchone()
+# print(myresult)
+#
+# # Select With a Filter
+# sql = "SELECT * FROM customers WHERE name = 'sundus'"
+# mycursor.execute(sql)
+# myresult = mycursor.fetchall()
+# for x in myresult:
+#     print(x)
+#
+# # Wildcard Characters
+# sql = "SELECT * FROM customers WHERE address Like '%way%'"
+# mycursor.execute(sql)
+# myresult = mycursor.fetchall()
+# for x in myresult:
+#     print(x)
 
-# Check if Database Exists
-mycursor.execute("SHOW DATABASES")
-for x in mycursor:
-    print (x)
+# Day_72
 
-# Creating a Table
-mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
-
-# Check if Table Exists
-mycursor.execute("SHOW TABLES")
-for x in mycursor:
-    print (x)
-
-# Primary Key
-mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
-
-
-# Day_71
-
-# Insert Into Table
-
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = ("John", "Highway 21")
-mycursor.execute(sql, val)
-mydb.commit()
-print(mycursor.rowcount, "record inserted.")
-
-# Insert Multiple Rows
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = [
-  ('Peter', 'Lowstreet 4'),
-  ('Amy', 'Apple st 652'),
-  ('Hannah', 'Mountain 21'),
-  ('Michael', 'Valley 345'),
-  ('Sandy', 'Ocean blvd 2'),
-  ('Betty', 'Green Grass 1'),
-  ('Richard', 'Sky st 331'),
-  ('Susan', 'One way 98'),
-  ('Vicky', 'Yellow Garden 2'),
-  ('Ben', 'Park Lane 38'),
-  ('William', 'Central st 954'),
-  ('Chuck', 'Main Road 989'),
-  ('Viola', 'Sideway 1633')
-]
-
-mycursor.executemany(sql, val)
-mydb.commit()
-print(mycursor.rowcount, "was inserted.")
-
-# Get Inserted ID
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = ("Sundus", "Highway 21")
-mycursor.execute(sql, val)
-mydb.commit()
-print("1 record inserted, ID:", mycursor.lastrowid)
-
-# Select From
-mycursor.execute("SELECT * FROM customers")
-myresult = mycursor.fetchall()
-for x in myresult:
-    print(x)
-
-# Selecting Columns
-mycursor.execute("SELECT name, address FROM customers")
-myresult = mycursor.fetchall()
-for x in myresult:
-    print(x)
-
-# fetchone() Method
-mycursor.execute("SELECT * FROM customers")
-myresult = mycursor.fetchone()
-print(myresult)
-
-# Select With a Filter
-sql = "SELECT * FROM customers WHERE name = 'sundus'"
+# Sort the Result
+sql = "SELECT * FROM customers ORDER BY name"
 mycursor.execute(sql)
 myresult = mycursor.fetchall()
 for x in myresult:
     print(x)
 
-# Wildcard Characters
-sql = "SELECT * FROM customers WHERE address Like '%way%'"
+# ORDER BY DESC
+sql = "SELECT * FROM customers ORDER BY name DESC"
 mycursor.execute(sql)
 myresult = mycursor.fetchall()
 for x in myresult:
     print(x)
+
+# Delete Record
+sql = "delete from customers where address = 'Mountain 21'"
+mycursor.execute(sql)
+mydb.commit()
+print(mycursor.rowcount, "records delete")
+
+# Delete a Table
+sql = "drop table customers"
+mycursor.execute(sql)
+
