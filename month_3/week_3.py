29353eaba93f6d4afb7ade79247a37328e7e7fef
sundus <s.alghernas1414@hotmail.com>
2019-10-29 21:58:31 +0300

    last update

--- month_3/week_3.py
+++ month_3/week_3.py
@@ -45,3 +45,44 @@ else:
 # Delete Folder
 
 os.rmdir("myfolder")
+
+
+# day_70
+
+import mysql.connector
+
+# creating a connection
+
+mydb = mysql.connector.connect(
+
+    host="localhost",
+    user="root",
+    passwd="12345678",
+    db="mydatabase"
+)
+print(mydb)
+
+# Creating a Database
+
+mycursor = mydb.cursor()
+mycursor.execute("CREATE DATABASE mydatabase")
+
+# Check if Database Exists
+
+mycursor.execute("SHOW DATABASES")
+for x in mycursor:
+    print (x)
+
+# Creating a Table
+
+mycursor = mydb.cursor()
+mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+
+# Check if Table Exists
+
+mycursor.execute("SHOW TABLES")
+for x in mycursor:
+    print (x)
+# Primary Key
+
+mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
